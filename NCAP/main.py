2cb0d9d037cfe115e7723aef3ed2d3f08405f1ea
Bbangddok's Kimbab <37821749+yoche2000@users.noreply.github.com>
2022-11-07 16:17:03 +0800

    Update main.py

--- NCAP/main.py
+++ NCAP/main.py
@@ -1,23 +1,9 @@
-"""
-API user 1
-usr = '635f76d255fe214928391bda'
-pwd = 'ac13f4f402733ae6e98575b785c85fb'
-
-API user 2
-usr = '635f80370c606d2eddb54bb3'
-pwd = 'd659b4d8fa50b1414d4527c151994cf9'
-
-API user 3
-usr = '635fa185ca93a25bd60ea721'
-pwd = 'f8e539277262f572ae0e734d657e015c'
-"""
-
 from p1451 import NCAP
 
 if __name__ == '__main__':
 
-    usr = '635fa185ca93a25bd60ea721'
-    pwd = 'f8e539277262f572ae0e734d657e015c'
+    usr = ''        #Put your username here
+    pwd = ''        #Put your credential here
 
     ncap = NCAP(ep='https://api.sigfox.com/v2/',
                 usr=usr,
